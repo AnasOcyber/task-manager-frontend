c6967855c6baaf4f9bc0cce9d1dc4480a41bdd03
Anas Obaid <anasobaidpk@gmail.com>
2024-09-10 21:42:33 +0500

    Fetching tasks from database

--- src/components/TaskBox.tsx
+++ src/components/TaskBox.tsx
@@ -1,14 +1,19 @@
 import { Box, Heading, HStack, Text } from "@chakra-ui/react";
 
-const TaskBox = () => {
+interface Props {
+  title: string;
+  description: string;
+}
+
+const TaskBox = ({ title, description }: Props) => {
   return (
     <Box w="100%" p={4} rounded="lg" ring={1} width={500} maxWidth={800}>
       <HStack justifyContent="space-between" gap={10}>
         <Box>
           <Heading fontSize={18} fontWeight="semibold">
-            Task Title
+            {title}
           </Heading>
-          <Text>Task description</Text>
+          <Text>{description}</Text>
         </Box>
       </HStack>
     </Box>
--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -6,10 +6,31 @@ import {
   Tabs,
   VStack,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import CreateTaskModal from "../components/CreateTaskModal";
 import TaskBox from "../components/TaskBox";
 
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  isCompleted: boolean;
+}
+
 const TasksPage = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const completedTasks = tasks.filter((t) => t.isCompleted);
+  const pendingTasks = tasks.filter((t) => !t.isCompleted);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/tasks")
+      .then((res) => setTasks(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <VStack gap={5}>
       <Tabs variant="soft-rounded" colorScheme="green" isFitted>
@@ -20,13 +41,24 @@ const TasksPage = () => {
         <TabPanels>
           <TabPanel>
             <VStack>
-              <TaskBox />
-              <TaskBox />
+              {completedTasks.map((task) => (
+                <TaskBox
+                  key={task.id}
+                  title={task.title}
+                  description={task.description}
+                />
+              ))}
             </VStack>
           </TabPanel>
           <TabPanel>
             <VStack>
-              <TaskBox />
+              {pendingTasks.map((task) => (
+                <TaskBox
+                  key={task.id}
+                  title={task.title}
+                  description={task.description}
+                />
+              ))}
             </VStack>
           </TabPanel>
         </TabPanels>
