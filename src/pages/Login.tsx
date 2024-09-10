19f473b5dd63be00b290618bf378778362b8d0a8
Anas Obaid <anasobaidpk@gmail.com>
2024-09-10 15:17:19 +0500

    Create login and register form component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,48 @@
-const Login = () => {
-  return <div>Login</div>;
-};
+import {
+  VStack,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Text,
+} from "@chakra-ui/react";
+import { useRef, FormEvent } from "react";
+import { Link } from "react-router-dom";
 
+const Login = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const user = {
+      email: emailRef.current?.value,
+      password: passwordRef.current?.value,
+    };
+    console.log(user); //TODO: Send user details to the backend
+  };
+
+  return (
+    <VStack>
+      <form onSubmit={handleSubmit}>
+        <VStack width={360}>
+          <FormControl marginBottom={3}>
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" ref={emailRef} />
+          </FormControl>
+          <FormControl marginBottom={5}>
+            <FormLabel>Password</FormLabel>
+            <Input type="password" ref={passwordRef} />
+          </FormControl>
+          <Button type="submit" colorScheme="green">
+            Log in
+          </Button>
+          <Text>
+            New user? <Link to="/register">Register</Link>
+          </Text>
+        </VStack>
+      </form>
+    </VStack>
+  );
+};
 export default Login;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,55 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { FormEvent, useRef } from "react";
+import { Link } from "react-router-dom";
+
 const Register = () => {
-  return <div>Register</div>;
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const user = {
+      name: nameRef.current?.value,
+      email: emailRef.current?.value,
+      password: passwordRef.current?.value,
+    };
+    console.log(user); //TODO: Send user details to the backend
+  };
+
+  return (
+    <VStack>
+      <form onSubmit={handleSubmit}>
+        <VStack width={360}>
+          <FormControl marginBottom={3}>
+            <FormLabel>First Name</FormLabel>
+            <Input type="text" ref={nameRef} />
+          </FormControl>
+          <FormControl marginBottom={3}>
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" ref={emailRef} />
+          </FormControl>
+          <FormControl marginBottom={5}>
+            <FormLabel>Password</FormLabel>
+            <Input type="password" ref={passwordRef} />
+          </FormControl>
+          <Button type="submit" colorScheme="green">
+            Create Account
+          </Button>
+          <Text>
+            Existing user? <Link to="/login">Login</Link>
+          </Text>
+        </VStack>
+      </form>
+    </VStack>
+  );
 };
 
 export default Register;
